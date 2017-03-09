0a630bed0b185548cdfc1d8bccfda25f27cdb877
Bruno Andrade <bruno.faria.andrade@gmail.com>
2017-03-09 03:40:14 -0300

    Alterações na classe Timer

--- Common/Timer.js
+++ Common/Timer.js
@@ -1,9 +1,23 @@
-function Timer(){
+function Timer(interval, repeat){
 
+    var _interval = interval || 1000;
+    Object.defineProperty(this, 'interval', {
+        get : function(){ return _interval; },
+        set : function(interval){
+            _interval = interval > 0 ? interval : 0;
+            this._iInterval = interval / 1000;
+        }
+    });
+
+    this.interval = _interval;
+    this.repeat = repeat || Infinity;
+
+    this._intervalCount = 0;
     this.count = 0;
 
     this.started = false;
     this.paused = false;
+    this.tick = false;
 
 }
 Timer.prototype.constructor = Timer;
@@ -11,12 +25,23 @@ Timer.prototype.constructor = Timer;
 Timer.prototype.start = function(dt){
 
     if(this.paused)
-        return true;
+        return false;
 
     this.started = true;
 
-    this.count += dt;
+    this._intervalCount += dt;
 
+    this.tick = false;
+
+    if(this.count == this.repeat)
+        this.pause();
+
+    if(this._intervalCount > this._iInterval){
+
+        this._intervalCount = 0;
+        this.count++;
+        this.tick = true;
+    }
 }
 
 Timer.prototype.pause = function(){
