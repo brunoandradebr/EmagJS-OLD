e6016af3e3d0cdd1660177552a366f91d488c7c3
Bruno Andrade <bruno.faria.andrade@gmail.com>
2017-03-13 20:22:23 -0300

    Adicionado classe de animação tween

---
+++ Common/Timeline.js
@@ -0,0 +1,81 @@
+function Timeline(loop, callback, repeat){
+
+    this.tweens = [];
+    this._tweenIndex = 0;
+    this.loop = loop || false;
+    this.callback = callback || undefined;
+    this.repeat = repeat || 1;
+    this._repeatCount = 0;
+
+}
+Timeline.prototype.constructor = Timeline;
+
+Timeline.prototype.add = function(label, frames){
+
+    var tween = {
+        label : label,
+        frames : frames
+    };
+    this.tweens.push(tween);
+}
+
+Timeline.prototype.reset = function(){
+    for(var i in this.tweens){
+
+        var tween = this.tweens[i];
+
+        tween.frames.forEach(function(frame){
+
+            frame.startTime = window.performance.now();
+            
+        });
+    }
+}
+
+Timeline.prototype.play = function(){
+
+    var _this = this;
+
+    if(!_this.tweens[_this._tweenIndex])
+        return false;
+
+    var currentTween = _this.tweens[_this._tweenIndex];
+    var currentTweenFrames = currentTween.frames;
+    var currentTweenFramesLength = currentTweenFrames.length;
+    
+    var totalFramesPlayed = 0;
+    currentTweenFrames.forEach(function(frame){
+
+        frame.play();
+
+        if(frame.completed){
+            totalFramesPlayed++;
+        }
+
+        if(totalFramesPlayed == currentTweenFramesLength){
+
+            if(_this.loop || _this.repeat > 1)
+                frame.completed = false;
+            
+            _this._tweenIndex++;
+            _this.reset();
+
+            // completed animation
+            if(!_this.tweens[_this._tweenIndex]){
+
+                _this._repeatCount++;
+
+                if(_this._repeatCount < _this.repeat)
+                    _this._tweenIndex = 0;
+                
+                if(_this.loop)
+                    _this._tweenIndex = 0;
+
+                if(_this.callback)
+                    _this.callback();
+                
+            }
+        }
+
+    });
+}
