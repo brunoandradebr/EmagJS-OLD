5f55ca29bf702af8802fbd8ce2a9213a5d9a2189
Bruno Andrade <bruno.faria.andrade@gmail.com>
2017-03-14 10:39:02 -0300

    Melhorado a classe Timer

--- Common/Timer.js
+++ Common/Timer.js
@@ -1,53 +1,68 @@
-function Timer(interval, repeat){
+function Timer(interval){
 
-    var _interval = interval || 1000;
-    Object.defineProperty(this, 'interval', {
-        get : function(){ return _interval; },
-        set : function(interval){
-            _interval = interval > 0 ? interval : 0;
-            this._iInterval = interval / 1000;
+    this.interval = interval || 1000;
+    this.startTime = this.firstTime = window.performance.now();
+    this.dt = 0;
+    
+    this.count = 0;
+
+    Object.defineProperty(this, 'tick', {
+        get : function(){
+            this.update();
+            return this.count;
         }
     });
 
-    this.interval = _interval;
-    this.repeat = repeat || Infinity;
-
-    this._intervalCount = 0;
-    this.count = 0;
-
     this.started = false;
-    this.paused = false;
-    this.tick = false;
+    this.paused = true;
+
+    this._start();
 
 }
 Timer.prototype.constructor = Timer;
 
-Timer.prototype.start = function(dt){
-
-    if(this.paused)
-        return false;
+Timer.prototype._start = function(){
 
     this.started = true;
+    this.paused = false;
 
-    this._intervalCount += dt;
-
-    this.tick = false;
-
-    if(this.count == this.repeat)
-        this.pause();
-
-    if(this._intervalCount > this._iInterval){
-
-        this._intervalCount = 0;
-        this.count++;
-        this.tick = true;
-    }
 }
 
 Timer.prototype.pause = function(){
+
     this.paused = true;
+
+}
+
+Timer.prototype.reset = function(){
+
+    this.count = 0;
+    this.startTime = window.performance.now();
+
 }
 
 Timer.prototype.resume = function(){
+
+    this.startTime = window.performance.now();
     this.paused = false;
+
+}
+
+Timer.prototype.update = function(){
+
+    if(!this.paused){
+
+        this.dt = window.performance.now() - this.startTime;
+
+        if(this.dt > this.interval){
+            this.count++;
+            this.startTime = window.performance.now();
+        }
+
+    }
+
+}
+
+Timer.prototype.elapsed = function(){
+    return (window.performance.now() - this.firstTime | 0);
 }
