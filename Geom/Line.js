d33f87adf46145a48d4ca8516e7163d0f8d6999b
Bruno Andrade <bruno.faria.andrade@gmail.com>
2017-03-31 11:01:02 -0300

    Adicionado a classe Line

---
+++ Geom/Line.js
@@ -0,0 +1,23 @@
+function Line(start, end, strokeWidth, strokeColor){
+    this.start = start;
+    this.end = end;
+    this.strokeColor = strokeColor || '#000';
+    this.strokeWidth = strokeWidth || 1;
+}
+Line.prototype.constructor = Line;
+
+Line.fromAngle = function(start, angle, length, strokeWidth, strokeColor){
+    return new Line(start, Vector.fromAngle(angle, length), strokeWidth, strokeColor);
+}
+
+Line.prototype.draw = function(graphics){
+
+    graphics.strokeStyle = this.strokeColor;
+    graphics.lineWidth = this.strokeWidth;
+    graphics.lineCap = 'round';
+    graphics.beginPath();
+    graphics.moveTo(this.start.x, this.start.y);
+    graphics.lineTo(this.start.x + this.end.x, this.start.y + this.end.y);
+    graphics.stroke();
+
+}
